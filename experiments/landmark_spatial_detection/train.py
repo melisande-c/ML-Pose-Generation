65960d718e87f3c43d63ead22d0b65bdb3c9bdef
melisande-c <milly.croft@gmail.com>
2023-05-04 19:35:44 +0100

    feat: complete training script

--- experiments/landmark_spatial_detection/train.py
+++ experiments/landmark_spatial_detection/train.py
@@ -1,7 +1,9 @@
 import sys
 from pathlib import Path
+import argparse
 
-sys.path.insert(0, str(Path(__file__).parent.parent.parent))
+print(str(Path(__file__).parents[2]))
+sys.path.insert(0, str(Path(__file__).parents[2]))
 
 import numpy as np
 import torch
@@ -9,24 +11,79 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 import matplotlib.pyplot as plt
-from models.VGGMod import VGGMod
-from data.dataset import FrameKeyPointDataset, Rescale, RandomSquareCrop
+from src.models.VGGMod import VGGMod
+from src.data.dataset import FrameKeyPointDataset, Rescale, RandomSquareCrop
+from src.utils.ModelTrainer import ModelTrainer
+from src.utils.PerformanceLogger import PandasPerformanceLogger
 
 
-# device = torch.device("mps")
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+class VGGTrainer(ModelTrainer):
+    # define how input sample to model
+    def output(self, sample):
+        return self.model(sample["img"])
 
-batch_size = 8
-epochs = 3
-ds_path = "/Users/milly/Documents/Personal/Projects/ML-Pose-Generation/dataset/Penn_Action"
+    # define how to calculate loss
+    def calc_loss(self, output, sample) -> torch.Tensor:
+        return self.loss(output, sample["labels"])
 
-vgg = torch.hub.load("pytorch/vision:v0.10.0", "vgg19", pretrained=True)
-model = VGGMod(vgg, 512, 13)
-model.to(device)
 
-dataset = FrameKeyPointDataset(
-    ds_path, transforms=[Rescale(512), RandomSquareCrop(512)]
-)
-dataloader = DataLoader(dataset, batch_size=int(batch_size), shuffle=True)
-optimiser = Adam(model.parameters(), lr=0.001)
-loss = nn.L1Loss()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-dsp", "--dataset_path", help="Path to dataset.", required=True
+    )
+    parser.add_argument(
+        "-sd",
+        "--save_dir",
+        help="Directory to save models and logs to.",
+        required=True,
+    )
+    parser.add_argument(
+        "-e", "--epochs", help="Number of Epochs to train for.", default=5
+    )
+    parser.add_argument(
+        "-bs", "--batch_size", help="No. of inputs per batch.", default=32
+    )
+    parser.add_argument(
+        "-lr",
+        "--learning_rate",
+        help="Learning rate of optimiser.",
+        default=0.001,
+    )
+    parser.add_argument(
+        "-tf",
+        "--training_fraction",
+        help="Fraction of dataset to use for training.",
+    )
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    vgg = torch.hub.load("pytorch/vision:v0.10.0", "vgg19", pretrained=True)
+    model = VGGMod(vgg, 512, 13)
+    model.to(device)
+
+    dataset = FrameKeyPointDataset(
+        args.dataset_path, transforms=[Rescale(512), RandomSquareCrop(512)]
+    )
+    optimiser = Adam(model.parameters(), lr=args.learning_rate)
+    loss = nn.L1Loss()
+
+    logger = PandasPerformanceLogger(args.save_dir / "performance.csv")
+
+    trainer = VGGTrainer(
+        model,
+        dataset,
+        optimiser,
+        loss,
+        args.save_dir,
+        args.batch_size,
+        train_fraction=args.train_fraction,
+        logger=logger,
+    )
+
+    trainer.run(args.epochs)
+
+
+if __name__ == "__main__":
+    main()
