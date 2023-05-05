115df794e9aa9ff1843765d6ea85f64eeb4db3cb
melisande-c <milly.croft@gmail.com>
2023-05-05 19:31:50 +0100

    fix(dataset): Rescale rounding error

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -107,7 +107,9 @@ class Rescale:
         axis = np.argmin(img.size)
         ratio = self.size / img.size[axis]
 
-        new_size = (int(img.size[1] * ratio), int(img.size[0] * ratio))
+        new_size = [int(img.size[1] * ratio), int(img.size[0] * ratio)]
+        new_size[axis] = self.size
+        new_size = tuple(new_size)
         img = transforms.Resize(new_size)(img)
 
         x = x * ratio
